e907ab0f1a4fae7517eba3826930aec3457f3bfa
Ariel de Llano <arieldellano@virgee.com>
2024-04-12 13:27:30 -0500

    Add tslib dependency and update theming.scss and index.ts files

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
 export * from './lib/components/button/button.component';
 export * from './lib/components/button/button.module';
+export * from './lib/theme.module';
---
+++ src/lib/theme.module.ts
@@ -0,0 +1,14 @@
+/**
+ * @license
+ * Copyright Virgee LLC. All Rights Reserved.
+ * Licensed under the MIT License. See LICENSE in the project root for license information.
+ */
+
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
+@NgModule({
+  imports: [CommonModule],
+  exports: [],
+})
+export class SVThemeModule {}
